1a6e1fc2ae0698495b7515dfe99b6c4601b4311e
Mahendra raut <mahendra.raut@payiq.net>
2023-05-01 20:04:56 +0530

    Initial Commit

---
+++ DoctorX/DoctorXApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorXApp.swift
+//  DoctorX
+//
+//  Created by Nayan Khadase on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DoctorXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
